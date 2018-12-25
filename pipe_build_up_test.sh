de65594e09d68973f1e36900b5216546f2ac3be8
Jon Jagger <jon@jaggersoft.com>
2018-12-25 05:47:35 +0000

    refactoring; drop unneeded [bundle exec] in up.sh

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -5,6 +5,5 @@ readonly SH_DIR="$( cd "$( dirname "${0}" )" && pwd )/sh"
 
 "${SH_DIR}/build_docker_images.sh"
 "${SH_DIR}/docker_containers_up.sh"
-if "${SH_DIR}/run_tests_in_containers.sh" "$@"; then
-  "${SH_DIR}/docker_containers_down.sh"
-fi
+"${SH_DIR}/run_tests_in_containers.sh" "$@"
+"${SH_DIR}/docker_containers_down.sh"
