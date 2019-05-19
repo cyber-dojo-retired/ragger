3e3d3bef1e18a4d52f4551edcb0d5bf72799d42a
Jon Jagger <jon@jaggersoft.com>
2019-05-19 21:03:13 +0100

    fix error introduced in refactoring

--- test_server/http_stub.rb
+++ test_server/http_stub.rb
@@ -5,7 +5,7 @@ class HttpStub
     @response = response
   end
 
-  def post(_hostname, _port, _method, _named_args)
+  def post(_hostname, _port, method, _named_args)
     { method.to_s => @response }
   end
 
