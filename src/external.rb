672d2c0b812e678d4870843140a954570daa8203
Jon Jagger <jon@jaggersoft.com>
2019-05-23 14:23:22 +0100

    refactoring; create external abstractions for http-net classes

--- src/external.rb
+++ src/external.rb
@@ -1,15 +1,20 @@
 require_relative 'http_json/request_packer'
 require_relative 'runner'
 require_relative 'stdout_log'
+require 'net/http'
 
 class External
 
   def initialize(options = {})
-    @http_tmp = options['http_tmp'] || HttpJson::RequestPacker.new
+    @http_get = Net::HTTP::Get
+    @http_post = Net::HTTP::Post
+    @http = Net::HTTP
+    @http_tmp = options['http_tmp'] || HttpJson::RequestPacker.new(self)
     @log = options['log'] || StdoutLog.new
     @runner = Runner.new(self)
   end
 
+  attr_reader :http_get, :http_post, :http
   attr_reader :http_tmp, :log, :runner
 
 end
--- src/http_json/request_packer.rb
+++ src/http_json/request_packer.rb
@@ -6,15 +6,19 @@ module HttpJson
 
   class RequestPacker
 
+    def initialize(external)
+      @external = external
+    end
+
     def get(hostname, port, path, args)
       packed(hostname, port, path, args) do |url|
-        Net::HTTP::Get.new(url)
+        http_get(url)
       end
     end
 
     def post(hostname, port, path, args)
       packed(hostname, port, path, args) do |url|
-        Net::HTTP::Post.new(url)
+        http_post(url)
       end
     end
 
@@ -25,8 +29,20 @@ module HttpJson
       req = yield uri
       req.content_type = 'application/json'
       req.body = JSON.generate(args)
-      service = Net::HTTP.new(uri.host, uri.port)
+      service = http(uri.host, uri.port)
       service.request(req)
+    end
+
+    def http_get(url)
+      @external.http_get.new(url)
+    end
+
+    def http_post(url)
+      @external.http_post.new(url)
+    end
+
+    def http(host, port)
+      @external.http.new(host, port)
     end
 
   end
