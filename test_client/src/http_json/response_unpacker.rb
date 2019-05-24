5a1c8e8ece898424dac1e992bbb36669618d0193
Jon Jagger <jon@jaggersoft.com>
2019-05-24 17:10:04 +0100

    refactoring; delete dead code in client-side tests

--- test_client/src/http_json/response_unpacker.rb
+++ test_client/src/http_json/response_unpacker.rb
@@ -13,11 +13,6 @@ module HttpJson
       unpacked(response.body, path.to_s)
     end
 
-    def post(path, args)
-      response = @requester.post(path, args)
-      unpacked(response.body, path.to_s)
-    end
-
     private
 
     def unpacked(body, path)
