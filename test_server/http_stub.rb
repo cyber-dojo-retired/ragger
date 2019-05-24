4df6ac9b287e784a6a716cfa43299b11d1e8922f
Jon Jagger <jon@jaggersoft.com>
2019-05-24 17:17:11 +0100

    refactoring; delete unneeded expression

--- test_server/http_stub.rb
+++ test_server/http_stub.rb
@@ -2,11 +2,11 @@ require 'ostruct'
 
 class HttpStub
 
-  def initialize(_host,_port)
+  def initialize(_hostname, _port)
   end
 
   def self.stub_request(json)
-    self.define_method(:request) do |_req|
+    define_method(:request) do |_req|
       OpenStruct.new(:body => JSON.generate(json))
     end
   end
