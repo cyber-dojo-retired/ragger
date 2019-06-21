b64bdad3c3bc26ac6549d0d55d0cf9e9e61802bd
Jon Jagger <jon@jaggersoft.com>
2019-06-21 08:32:22 +0100

    take out profiling until it can be flushed properly

--- test_client/test/util/run.sh
+++ test_client/test/util/run.sh
@@ -11,16 +11,7 @@ readonly SCRIPT="([ '${MY_DIR}/coverage.rb' ] + %w(${TEST_FILES[*]})).each{ |fil
 
 #export RUBYOPT=-w
 
-echo "${SCRIPT}" > /tmp/test_run.rb
-ruby-prof \
-   --min_percent=0.05 \
-   --printer=flat \
-   --file=${COVERAGE_ROOT}/profiled.test_run.dump \
-   /tmp/test_run.rb \
-   ${TEST_ARGS[@]} | tee ${TEST_LOG}
-
-sleep 1
-echo 'flush...' >> ${TEST_LOG}
+ruby -e "${SCRIPT}" -- ${TEST_ARGS[@]} | tee ${TEST_LOG}
 
 ruby ${MY_DIR}/check_test_results.rb \
   ${TEST_LOG} \
--- test_server/util/run.sh
+++ test_server/util/run.sh
@@ -11,18 +11,25 @@ readonly SCRIPT="([ '${MY_DIR}/coverage.rb' ] + %w(${TEST_FILES[*]})).each{ |fil
 
 #export RUBYOPT=-w
 
-echo "${SCRIPT}" > /tmp/test_run.rb
-ruby-prof \
-   --min_percent=0.05 \
-   --printer=flat \
-   --file=${COVERAGE_ROOT}/profiled.test_run.dump \
-   /tmp/test_run.rb \
-   ${TEST_ARGS[@]} | tee ${TEST_LOG}
-
-sleep 1
-echo 'flush...' >> ${TEST_LOG}
+ruby -e "${SCRIPT}" -- ${TEST_ARGS[@]} | tee ${TEST_LOG}
 
 ruby ${MY_DIR}/check_test_results.rb \
   ${TEST_LOG} \
   ${COVERAGE_ROOT}/index.html \
     > ${COVERAGE_ROOT}/done.txt
+
+
+# I tried to add profiling.
+# cyberdojo/rack-base installs ruby-prof gem.
+#
+#echo "${SCRIPT}" > /tmp/test_run.rb
+#ruby-prof \
+#   --min_percent=0.05 \
+#   --printer=flat \
+#   --file=${COVERAGE_ROOT}/profiled.test_run.dump \
+#   /tmp/test_run.rb \
+#   ${TEST_ARGS[@]} | tee ${TEST_LOG}
+
+# This fails and the reason seems to be that TEST_LOG
+# is being buffered so is not fully written when the
+# check_test_results.rb script runs.
