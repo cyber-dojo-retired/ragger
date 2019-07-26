35195b8d0bb8b3dbf2e63c566377f0b61fd6cd8f
cron <cron@cyber-dojo.org>
2019-07-26 15:12:54 +0100

    use versioner's .env file to set TAG for runner

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -3,6 +3,14 @@ set -e
 
 readonly SH_DIR="$( cd "$( dirname "${0}" )" && pwd )/sh"
 
+docker run --rm \
+  cyberdojo/versioner:${CYBER_DOJO_VERSIONER_TAG:-latest} \
+    sh -c 'cat /app/.env' \
+      > /tmp/versioner.web.env
+set -a
+. /tmp/versioner.web.env
+set +a
+
 "${SH_DIR}/build_docker_images.sh"
 "${SH_DIR}/docker_containers_up.sh"
 "${SH_DIR}/run_tests_in_containers.sh" "$@"
