044ae168642da0e14e24ff2ec30b534cc8b8f4d0
JonJagger <jon@jaggersoft.com>
2019-07-29 06:40:33 +0100

    tag locally built ragger image so you can more easily run local integration tests

--- sh/build_docker_images.sh
+++ sh/build_docker_images.sh
@@ -6,4 +6,17 @@ export SHA=$(cd "${ROOT_DIR}" && git rev-parse HEAD)
 
 docker-compose \
   --file "${ROOT_DIR}/docker-compose.yml" \
-    build
+    build \
+      ragger
+
+echo
+docker-compose \
+  --file "${ROOT_DIR}/docker-compose.yml" \
+    build \
+      ragger-client
+
+# Assuming we do not have any new ragger commits, ragger's latest commit
+# sha will match the image tag inside versioner's .env file.
+# This means we can tag to it and a [cyber-dojo up] call
+# will use the tagged image.
+docker tag cyberdojo/ragger:latest cyberdojo/ragger:${SHA:0:7}
