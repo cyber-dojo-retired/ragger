806c31139c1c65ce3b07e6bd47b79385cd635f09
Jon Jagger <jon@jaggersoft.com>
2019-12-20 08:23:36 +0000

    Refactoring; collapse script duplication

--- sh/run_tests_in_containers.sh
+++ sh/run_tests_in_containers.sh
@@ -1,66 +1,58 @@
 #!/bin/bash
 
-declare server_status=0
-declare client_status=0
-
-readonly ROOT_DIR="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
-readonly MY_NAME=ragger
-
-readonly SERVER_NAME="test-${MY_NAME}-server"
-readonly CLIENT_NAME="test-${MY_NAME}-client"
-
-readonly COVERAGE_ROOT=/tmp/coverage
+readonly root_dir="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
+readonly my_name=ragger
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - -
+run_tests()
+{
+  local -r coverage_root=/tmp/coverage
+  local -r user="${1}"
+  local -r test_dir="test_${2}"
+  local -r container_name="test-${my_name}-${2}"
 
+  docker exec \
+    --user "${user}" \
+    --env COVERAGE_ROOT=${coverage_root} \
+    "${container_name}" \
+      sh -c "/app/test/util/run.sh ${@:3}"
+
+  local -r status=$?
+
+  # You can't [docker cp] from a tmpfs,
+  # so tar-piping coverage out.
+  docker exec \
+    "${container_name}" \
+    tar Ccf \
+      "$(dirname "${coverage_root}")" \
+      - "$(basename "${coverage_root}")" \
+        | tar Cxf "${root_dir}/${test_dir}/" -
+
+  printf "Coverage report copied to ${test_dir}/coverage/\n"
+  return ${status}
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - -
+declare server_status=0
 run_server_tests()
 {
-  docker exec \
-    --user nobody \
-    --env COVERAGE_ROOT=${COVERAGE_ROOT} \
-    "${SERVER_NAME}" \
-      sh -c "/app/test/util/run.sh ${*}"
-
+  run_tests nobody server "${*}"
   server_status=$?
-
-  # You can't [docker cp] from a tmpfs, you have to tar-pipe out.
-  docker exec "${SERVER_NAME}" \
-    tar Ccf \
-      "$(dirname "${COVERAGE_ROOT}")" \
-      - "$(basename "${COVERAGE_ROOT}")" \
-        | tar Cxf "${ROOT_DIR}/test_server/" -
-
-  echo "Coverage report copied to ${MY_NAME}/test_server/coverage/"
 }
 
-# - - - - - - - - - - - - - - - - - - - - - - - - - -
-
+declare client_status=0
 run_client_tests()
 {
-  docker exec \
-    --user nobody \
-    --env COVERAGE_ROOT=${COVERAGE_ROOT} \
-    "${CLIENT_NAME}" \
-      sh -c "/app/test/util/run.sh ${*}"
-
+  run_tests nobody client "${*}"
   client_status=$?
-
-  # You can't [docker cp] from a tmpfs, you have to tar-pipe out.
-  docker exec "${CLIENT_NAME}" \
-    tar Ccf \
-      "$(dirname "${COVERAGE_ROOT}")" \
-      - "$(basename "${COVERAGE_ROOT}")" \
-        | tar Cxf "${ROOT_DIR}/test_client/" -
-
-  echo "Coverage report copied to ${MY_NAME}/test_client/coverage/"
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-if [ "$1" = "server" ]; then
+printf '\n'
+if [ "${1}" == "server" ]; then
   shift
   run_server_tests "$@"
-elif [ "$1" = "client" ]; then
+elif [ "${1}" == "client" ]; then
   shift
   run_client_tests "$@"
 else
@@ -68,14 +60,13 @@ else
   run_client_tests "$@"
 fi
 
-if [[ ( ${server_status} == 0 && ${client_status} == 0 ) ]];  then
-  echo "------------------------------------------------------"
-  echo "All passed"
+if [ "${server_status}" == "0" ] && [ "${client_status}" == "0" ];  then
+  echo All passed
   exit 0
 else
-  echo
-  echo "server: ${SERVER_NAME}, status = ${server_status}"
-  echo "client: ${CLIENT_NAME}, status = ${client_status}"
-  echo
-  exit 1
+  printf '\n'
+  printf "test-${my_name}-server: status = ${server_status}\n"
+  printf "test-${my_name}-client: status = ${client_status}\n"
+  printf '\n'
+  exit 42
 fi
