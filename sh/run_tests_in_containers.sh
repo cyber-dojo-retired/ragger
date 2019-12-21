058bc8a6168f2436ccfc3837ec25a522d7c508f1
Jon Jagger <jon@jaggersoft.com>
2019-12-21 20:13:58 +0000

    Simplify .sh file

--- sh/run_tests_in_containers.sh
+++ sh/run_tests_in_containers.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 readonly root_dir="$( cd "$( dirname "${0}" )" && cd .. && pwd )"
 readonly my_name=ragger
@@ -11,13 +12,14 @@ run_tests()
   local -r test_dir="test_${2}"
   local -r container_name="test-${my_name}-${2}"
 
+  set +e
   docker exec \
     --user "${user}" \
     --env COVERAGE_ROOT=${coverage_root} \
     "${container_name}" \
       sh -c "/app/test/util/run.sh ${@:3}"
-
   local -r status=$?
+  set -e
 
   # You can't [docker cp] from a tmpfs,
   # so tar-piping coverage out.
@@ -28,27 +30,17 @@ run_tests()
       - "$(basename "${coverage_root}")" \
         | tar Cxf "${root_dir}/${test_dir}/" -
 
-  printf "Coverage report copied to ${test_dir}/coverage/\n"
+  echo "Coverage report copied to ${test_dir}/coverage/"
+  echo "${2} test status == ${status}"
   return ${status}
 }
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - -
-declare server_status=0
-run_server_tests()
-{
-  run_tests nobody server "${*}"
-  server_status=$?
-}
-
-declare client_status=0
-run_client_tests()
-{
-  run_tests nobody client "${*}"
-  client_status=$?
-}
+run_server_tests() { run_tests nobody server "${*}"; }
+run_client_tests() { run_tests nobody client "${*}"; }
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - -
-printf '\n'
+echo
 if [ "${1}" == 'server' ]; then
   shift
   run_server_tests "$@"
@@ -59,14 +51,4 @@ else
   run_server_tests "$@"
   run_client_tests "$@"
 fi
-
-if [ "${server_status}" == '0' ] && [ "${client_status}" == '0' ];  then
-  echo All passed
-  exit 0
-else
-  echo
-  echo "test-${my_name}-server: status = ${server_status}"
-  echo "test-${my_name}-client: status = ${client_status}"
-  echo
-  exit 42
-fi
+echo All passed
