946b11610a83fcad3b4a5a34df557a0ac0fcd0b6
Jon Jagger <jon@jaggersoft.com>
2019-12-22 14:16:27 +0000

    Comment out profiling till needed

--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 readonly MY_DIR="$( cd "$( dirname "${0}" )" && pwd )"
 readonly TYPE="${1}" # client|server
@@ -9,23 +10,15 @@ readonly TEST_LOG=${COVERAGE_ROOT}/test.log
 readonly TEST_LOG_PART=${COVERAGE_ROOT}/test.log.part
 
 readonly SCRIPT="
-require 'ruby-prof'
-GC.disable
-RubyProf.start
 require '${MY_DIR}/coverage.rb'
 %w(${TEST_FILES[*]}).shuffle.each{ |file|
   require file
-}
-result = RubyProf.stop
-def print_profile(result, kind, name)
-  kind.new(result).print(File.open(\"${COVERAGE_ROOT}/profile.#{name}.log\",'w+'))
-end
-print_profile(result, RubyProf::FlatPrinter, 'flat')
-#print_profile(result, RubyProf::GraphPrinter, 'graph')
-#print_profile(result, RubyProf::CallStackPrinter, 'call_stack')"
+}"
 
 export RUBYOPT='-W2'
 mkdir -p ${COVERAGE_ROOT}
+
+set +e
 
 ruby -e "${SCRIPT}" -- ${TEST_ARGS[@]} \
   2>&1 | tee ${TEST_LOG} ${TEST_LOG_PART}
@@ -37,3 +30,16 @@ ruby ${MY_DIR}/check_test_results.rb \
     2>&1 | tee -a ${TEST_LOG}
 
 exit ${PIPESTATUS[0]}
+
+#Profiling?
+#-----------
+#require 'ruby-prof'
+#GC.disable
+#RubyProf.start
+#result = RubyProf.stop
+#def print_profile(result, kind, name)
+#  kind.new(result).print(File.open(\"${COVERAGE_ROOT}/profile.#{name}.log\",'w+'))
+#end
+#print_profile(result, RubyProf::FlatPrinter, 'flat')
+#print_profile(result, RubyProf::GraphPrinter, 'graph')
+#print_profile(result, RubyProf::CallStackPrinter, 'call_stack')"
